503de526fb1c3884f411b0fa02b9b6b46b035101
FrYt4 <adrian.fryczkie@yahoo.com>
2024-06-02 18:01:11 +0200

    a

--- src/redux/selectors.js
+++ src/redux/selectors.js
@@ -10,6 +10,19 @@ export const selectError = state => state.contacts.error;
 export const selectFilteredContacts = createSelector(
   [selectContacts, selectFilter],
   (contacts, filter) => {
+    console.log('Contacts:', contacts);
+    console.log('Filter:', filter);
+
+    if (!Array.isArray(contacts)) {
+      console.error('Contacts is not an array:', contacts);
+      return [];
+    }
+
+    if (typeof filter !== 'string') {
+      console.error('Filter is not a string:', filter);
+      return contacts;
+    }
+
     return contacts.filter(contact =>
       contact.name.toLowerCase().includes(filter.toLowerCase())
     );
