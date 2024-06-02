701e790d11e41c3db69aded4e603a5e372e5b68a
FrYt4 <adrian.fryczkie@yahoo.com>
2024-06-02 17:55:57 +0200

    a

--- src/components/filter.jsx
+++ src/components/filter.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import PropTypes from 'prop-types'; 
 import { useDispatch } from 'react-redux';
 import { setFilter } from '../redux/filterSlice';
 import { nanoid } from '@reduxjs/toolkit';
@@ -25,6 +24,3 @@ return (
   );
 }
 
-Filter.propTypes = {
-    onChange: PropTypes.func.isRequired,
-};
